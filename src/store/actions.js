196a5a4bacc97233682421155a889f0d1b52ccf8
wujianw <362182823@qq.comm>
2016-10-19 22:42:41 +0800

    root

--- src/store/actions.js
+++ src/store/actions.js
@@ -4,9 +4,9 @@ import * as types from './mutation-types'
 export const loginMobile = ({ commit },option) => {
     console.log(option)
     commit(types.LOGIN_MOBILE_SUCCESS,option)
-    //member.getLoginInformation(
-    //    option,
-    //    data => commit(types.LOGIN_MOBILE_SUCCESS,data),
-    //    data => commit(types.LOGIN_MOBILE_FAILURE,data)
-    //)
+    member.getLoginInformation(
+        option,
+        data => commit(types.LOGIN_MOBILE_SUCCESS,data),
+        data => commit(types.LOGIN_MOBILE_FAILURE,data)
+    )
 }
