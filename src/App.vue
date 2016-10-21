f149cc41f94540bc6a7c1533ce42738a5240972c
积分宝 <1739224942@qq.com>
2016-10-21 16:33:10 +0800

    tsf2016-10-21

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="app">
+        <router-view></router-view>
+    </div>
+</template>
+<script>
+//    import userLoad from './components/loads'
+    export default {
+        components: {
+//            userLoad
+        }
+    }
+</script>
+<style>
+    html {
+        height: 100%;
+    }
+    body {
+        position:relative;
+        height: 100%;
+        background: #f2f2f2;
+    }
+    #app{
+        height:100%;
+    }
+    .flex-center{display: flex;align-items: center;justify-content: center;}
+    .flex-start{display: flex;align-items: center;justify-content: flex-start;}
+    .flex-space{display: flex;align-items: center;justify-content: space-between;}
+    .flex-end{display: flex;align-items: center;justify-content: flex-end;}
+    .flex-end-end{display: flex;align-items: flex-end;justify-content: flex-end;}
+    .flex-space-around{display: flex;align-items: center;justify-content: space-between;}
+</style>
